d459642c4496bf8279ea42851fe4271ba5b76dcd
Sujeevan18 <yogarajansujeevan2002@gmail.com>
2025-08-11 11:06:59 +0530

    data Validation partially created

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -1,32 +1,47 @@
 from datetime import datetime
 import os
-from networksecurity.constants import training_pipeline
+from Network_Security.networksecurity.constants.training_pipeline import _init_
 
-print(training_pipeline.PIPELINE_NAME)
-print(training_pipeline.ARTIFACT_DIR)
+print(_init_.PIPELINE_NAME)
+print(_init_.ARTIFACT_DIR)
 
 class TrainingPipelineConfig:
     def __init__(self, timestamp: datetime):
         timestamp = timestamp.strftime("%Y-%m-%d-%H-%M-%S")
-        self.pipeline_name = training_pipeline.PIPELINE_NAME
-        self.artifact_name = training_pipeline.ARTIFACT_DIR
+        self.pipeline_name = _init_.PIPELINE_NAME
+        self.artifact_name = _init_.ARTIFACT_DIR
         self.artifact_dir = os.path.join(self.artifact_name, timestamp)
         self.timestamp = timestamp
 
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_ingestion_dir:str=os.path.join(
-            training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
+            training_pipeline_config.artifact_dir,_init_.DATA_INGESTION_DIR_NAME
         )
         self.feature_store_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
+                self.data_ingestion_dir, _init_.DATA_INGESTION_FEATURE_STORE_DIR, _init_.FILE_NAME
             )
         self.training_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
+                self.data_ingestion_dir, _init_.DATA_INGESTION_INGESTED_DIR, _init_.TRAIN_FILE_NAME
             )
         self.testing_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
+                self.data_ingestion_dir, _init_.DATA_INGESTION_INGESTED_DIR, _init_.TEST_FILE_NAME
             )
-        self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
-        self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
-        self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
+        self.train_test_split_ratio: float = _init_.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+        self.collection_name: str = _init_.DATA_INGESTION_COLLECTION_NAME
+        self.database_name: str = _init_.DATA_INGESTION_DATABASE_NAME
+
+class DataValidationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, _init_.DATA_VALIDATION_DIR_NAME)
+        self.valid_data_dir: str = os.path.join(self.data_validation_dir, _init_.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir: str = os.path.join(self.data_validation_dir, _init_.DATA_VALIDATION_INVALID_DIR)
+        self.valid_train_file_path: str = os.path.join(self.valid_data_dir, _init_.TRAIN_FILE_NAME)
+        self.valid_test_file_path: str = os.path.join(self.valid_data_dir, _init_.TEST_FILE_NAME)
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, _init_.TRAIN_FILE_NAME)
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, _init_.TEST_FILE_NAME)
+        self.drift_report_file_path: str = os.path.join(
+            self.data_validation_dir,
+            _init_.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            _init_.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+        )
